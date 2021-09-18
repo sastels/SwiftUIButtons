15944575443e611b56cc40e3b6e88b409db368fb
Steve Astels <sastels@gmail.com>
2021-09-18 19:19:36 -0400

    custom buttons example

--- SwiftUIButtons/ContentView.swift
+++ SwiftUIButtons/ContentView.swift
@@ -8,14 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State var isUp = true
+
+  var body: some View {
+    ZStack {
+      Color(UIColor.systemGray5).ignoresSafeArea()
+
+      VStack(spacing: 20) {
+        HStack(spacing: 20) {
+          Button(action: {}) {
+            Text("Default")
+          }.buttonStyle(CustomButtonStyle())
+
+          Button(action: { isUp.toggle() }) {
+            Text("Light")
+          }.buttonStyle(CustomButtonStyle(.default(type: isUp ? .light : .success)))
+
+          Button(action: {}) {
+            Text("Success")
+          }.buttonStyle(CustomButtonStyle(.default(type: .success)))
+
+          Button(action: {}) {
+            Text("Info")
+          }.buttonStyle(CustomButtonStyle(.default(type: .info)))
+
+          Button(action: {}) {
+            Text("Warning")
+          }.buttonStyle(CustomButtonStyle(.default(type: .warning)))
+          Button(action: {}) {
+            Text("Danger")
+          }.buttonStyle(CustomButtonStyle(.default(type: .danger)))
+
+          Button(action: {}) {
+            Text("Dark")
+          }.buttonStyle(CustomButtonStyle(.default(type: .dark)))
+        }
+
+        HStack(spacing: 20) {
+          Button(action: {}) {
+            Text("Default")
+          }.buttonStyle(CustomButtonStyle(.default(type: .primary)))
+
+          Button(action: {}) {
+            Text("Transparent")
+          }.buttonStyle(CustomButtonStyle(.transparent(type: .primary)))
+
+          Button(action: {}) {
+            Text("Outline")
+          }.buttonStyle(CustomButtonStyle(.outline(type: .primary)))
+
+          Button(action: {}) {
+            Text("Rounded")
+          }.buttonStyle(CustomButtonStyle(.rounded(type: .primary)))
+        }
+
+        HStack(spacing: 20) {
+          Button(action: {}) {
+            Text("Small")
+          }.buttonStyle(CustomButtonStyle(size: .small))
+
+          Button(action: {}) {
+            Text("Default")
+          }.buttonStyle(CustomButtonStyle())
+
+          Button(action: {}) {
+            Text("Large")
+          }.buttonStyle(CustomButtonStyle(size: .large))
+        }
+        Button(action: {}) {
+          Text("FullWidth")
+        }.buttonStyle(CustomButtonStyle(isFullWidth: true))
+      }.padding()
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
