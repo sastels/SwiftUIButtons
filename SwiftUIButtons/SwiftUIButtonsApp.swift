a33556c5434b2d1c7f604507d2c5979aeeb21627
Steve Astels <sastels@gmail.com>
2021-09-18 15:17:01 -0400

    Initial Commit

---
+++ SwiftUIButtons/SwiftUIButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIButtonsApp.swift
+//  SwiftUIButtons
+//
+//  Created by Stephen Astels on 2021-09-18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
